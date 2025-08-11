f6e42f317b84582cafdf081fb775136799e1338e
comiee <1440667228@qq.com>
2025-08-11 22:02:21 +0800

    加具土命技能

--- extension/英雄/extension.js
+++ extension/英雄/extension.js
@@ -259,6 +259,38 @@ export default function () {
                         },
                         "_priority": 0,
                     },
+                    "加具土命": {
+                        audio: "ext:英雄/加具土命",
+                        mod: {
+                            targetInRange(card, player, target) {
+                                if (card.name === 'sha' && card.nature === "fire") {
+                                    return true;
+                                }
+                            }
+                        },
+                        enable: "phaseUse",
+                        usable: 1,
+                        filterCard: true,
+                        position: "hes",
+                        filterTarget(card, player, target) {
+                            if (ui.selected.targets.length === 0) {
+                                return target.hasMark("天照_mark");  // 第一个角色：有天照标记
+                            }
+                            if (ui.selected.targets.length === 1) {
+                                return target !== ui.selected.targets[0];  // 第二个角色：不与第一个角色相同
+                            }
+                            return true;
+                        },
+                        targetprompt: ["被移走", "移动目标"],
+                        selectTarget: 2,
+                        multitarget: true,
+                        async content(event, trigger, player) {
+                            event.targets[0].removeMark("天照_mark");
+                            event.targets[1].addMark("天照_mark");
+                            event.targets[1].addSkill("天照_mark");
+                        },
+                        "_priority": 0,
+                    },
                     "矢量偏转": {
                         audio: "ext:矢量:true",
                         trigger: {
@@ -380,6 +412,10 @@ export default function () {
                     "火遁_info": "你可以将红色牌当火杀使用或打出",
                     "月读": "月读",
                     "月读_info": "每回合限一次，你可以指定一名其他角色，与其同时展示一张牌，若你和他展示的牌颜色一致，你使其获得【读】状态（非锁定技失效且不能使用或打出牌），直到回合结束",
+                    "加具土命": "加具土命",
+                    "加具土命_info": "你使用的火杀无距离限制；出牌阶段，你可以弃一张牌，移动场上一枚【照】标记的位置",
+                    "千鸟": "千鸟",
+                    "千鸟_info": "每回合限一次，你可令一名其他角色展示一张手牌，若你交给其一张颜色不同的手牌，你对其造成一点伤害，并弃置其防具",
                     "矢量偏转": "矢量偏转",
                     "矢量偏转_info": "每回合限一次，当你受到伤害时，你可以弃一张牌并选择一名其他角色，将此伤害转移给对方",
                     "一方通行": "一方通行",
@@ -396,7 +432,12 @@ export default function () {
             forumURL: "",
             version: "1.0",
         },
-        files: {"character": ["巴泽特.jpg", "宇智波鼬.jpg", "一方通行.jpg", "宇智波佐助.jpg"], "card": [], "skill": [], "audio": []},
+        files: {
+            "character": ["巴泽特.jpg", "宇智波鼬.jpg", "一方通行.jpg", "宇智波佐助.jpg"],
+            "card": [],
+            "skill": [],
+            "audio": []
+        },
         connect: false
     }
 };
